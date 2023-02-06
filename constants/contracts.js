7f2bd4da295260f12f475df04b5d8c77ee90d06b
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-06 18:14:24 +0530

    Update contracts.js

--- constants/contracts.js
+++ constants/contracts.js
@@ -1,8 +1,5 @@
 const Contracts = {
-  // DataDAOFactory: "0x48Efc5Caa73E5B2716eb27Dc0b537B8600177a79",
-  DataDAOFactory: "0xE590D9C29282f805B0c4a90F9Cc1981DAc8B8F22",
+  DataDAOFactory: "0xBF431d2b3063aE771F51F0f281fd4c7CAf732717",
 };
 
 export default Contracts;
-
-// 0xC1be9082Ff815F1A126894DEa3c205f9C4903658
